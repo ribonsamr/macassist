c742b44081cd94c90d16358a61e5679edd51ffae
Amr <amressam01@hotmail.com>
2018-10-04 21:33:26 +0200

    Apply PEP8 Also use python3.7 for the sha-bang

--- ma_head.py
+++ ma_head.py
@@ -2,9 +2,10 @@ import sys
 from os import path, system
 from colorama import init, Fore, Back, Style
 from pprint import pprint
-import json, argparse
+import json
+import argparse
 
-init(autoreset=True) # colorama init
+init(autoreset=True)  # colorama init
 
 global data_file_path
 global data, data_values
@@ -16,13 +17,16 @@ bright_yellow = Fore.YELLOW + Style.BRIGHT
 
 main_input_message = underlinecode + ('command:') + endcode
 main_error_message = Fore.RED + 'Please choose a valid number.' + Fore.RESET
-main_open_message = bright_yellow + 'macAssist2' + Fore.WHITE + ' – Refreshed by amressam for 2018 –'
+main_open_message = bright_yellow + 'macAssist2' + \
+    Fore.WHITE + ' – Refreshed by amressam for 2018 –'
 
 data_file_path = path.dirname(path.realpath(__file__)) + '/data.json'
 
+
 def safely_strint(_str) -> bool or int:
-	try: return int(_str)
-	except Exception as e:
-		print("Error in safely_strint")
-		print(e)
-		return False
+    try:
+        return int(_str)
+    except Exception as e:
+        print("Error in safely_strint")
+        print(e)
+        return False
--- macassist.py
+++ macassist.py
@@ -1,126 +1,142 @@
-#!/usr/bin/env python3.6
-#macassist2 Fri Dec 15 11:30 PM
-#stable Sat Dec 16 3:48 AM
+#!/usr/bin/env python3.7
+# macassist2 Fri Dec 15 11:30 PM
+# stable Sat Dec 16 3:48 AM
 
 from ma_head import *
 from manager import run
 
+
 def wrap_string(_str, underline=False, front="", back=""):
-	final_string = ""
-	
-	if underline:
-		final_string += underlinecode
+    final_string = ""
 
-	final_string += front + _str + back + endcode
+    if underline:
+        final_string += underlinecode
 
-	return final_string
-	# return underlinecode if underline else "" + front + _str + back + endcode
+    final_string += front + _str + back + endcode
+
+    return final_string
+    # return underlinecode if underline else "" + front + _str + back + endcode
+
 
 def print_pure_json(width=200):
-	loadJSON()
-	pprint(data, width = width) # Look in pprint documentation
+    loadJSON()
+    pprint(data, width=width)  # Look in pprint documentation
+
 
 def print_the_data():
-	loadJSON()
-	print()
-	for i in range(len(data)):
-		print('  [{}]'.format(i+1), data[i])
+    loadJSON()
+    print()
+    for i in range(len(data)):
+        print('  [{}]'.format(i + 1), data[i])
+
 
 def loadJSON():
-	global data, data_values
+    global data, data_values
 
-	try:
-		with open(data_file_path) as file:
-			pure_data = json.load(file)
-			data = list(pure_data.keys())
-			data_values = list(pure_data.values())
-	except Exception as error:
-		print("Can't load data file.")
-		print(str(error))
-		exit(0) # temporary
+    try:
+        with open(data_file_path) as file:
+            pure_data = json.load(file)
+            data = list(pure_data.keys())
+            data_values = list(pure_data.values())
+    except Exception as error:
+        print("Can't load data file.")
+        print(str(error))
+        exit(0)  # temporary
+
 
 def early_init():
-	system('clear')
-	print(main_open_message)
-	loadJSON()
+    system('clear')
+    print(main_open_message)
+    loadJSON()
+
 
 def args_init(printArgs: bool = False) -> bool or list:
-	try:
-		parser = argparse.ArgumentParser()
-		parser.add_argument('commandnumber', type=int, nargs='*', help='Command numbers to quickly exceute.')
-		args = parser.parse_args()
-		if printArgs: print(args.commandnumber)
-		return args.commandnumber
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('commandnumber', type=int, nargs='*',
+                            help='Command numbers to quickly exceute.')
+        args = parser.parse_args()
+        if printArgs:
+            print(args.commandnumber)
+        return args.commandnumber
 
-	except Exception as e:
-		print("Error in args_init")
-		print(e)
-		return False
+    except Exception as e:
+        print("Error in args_init")
+        print(e)
+        return False
+
 
 def confirm_execution(command_name, command) -> bool:
-	while True:
-		print()
-		print('', wrap_string("Command name:", front=Fore.YELLOW) , wrap_string(command_name, front=Fore.CYAN))
-		print('', wrap_string("Command:", front=Fore.YELLOW), command)
-		print('\n', wrap_string("Do you want to exceute the command? [y/n] [yes]: ", front=Fore.RED), end='')
-		user_input = input()
-		if user_input == 'no' or user_input == 'n':
-			print()
-			return False
-		else:
-			return True
+    while True:
+        print()
+        print('', wrap_string("Command name:", front=Fore.YELLOW),
+              wrap_string(command_name, front=Fore.CYAN))
+        print('', wrap_string("Command:", front=Fore.YELLOW), command)
+        print('\n', wrap_string(
+            "Do you want to exceute the command? [y/n] [yes]: ", front=Fore.RED), end='')
+        user_input = input()
+        if user_input == 'no' or user_input == 'n':
+            print()
+            return False
+        else:
+            return True
+
 
 def executer(index):
-	user_choice = safely_strint(index)
+    user_choice = safely_strint(index)
 
-	if user_choice - 1 in range(len(data)):
-		command = data_values [user_choice - 1]
-		command_name = data [user_choice - 1]
-		
-		# if the user wants to execute, then do it.
-		if confirm_execution(command_name, command):
-			system(command)
-			print()
-	else:
-		print(wrap_string("Enter a valid number from 1 to {}.".format(len(data)), front=Fore.RED))
+    if user_choice - 1 in range(len(data)):
+        command = data_values[user_choice - 1]
+        command_name = data[user_choice - 1]
+
+        # if the user wants to execute, then do it.
+        if confirm_execution(command_name, command):
+            system(command)
+            print()
+    else:
+        print(wrap_string("Enter a valid number from 1 to {}.".format(
+            len(data)), front=Fore.RED))
+
 
 def main():
-	global data
-	early_init()
-	args = args_init(printArgs = False)
-	if args:
-		for i in args:
-			i = safely_strint(i)
-			executer(i)
-		exit(0)
+    global data
+    early_init()
+    args = args_init(printArgs=False)
+    if args:
+        for i in args:
+            i = safely_strint(i)
+            executer(i)
+        exit(0)
 
-	print_the_data()
-	print("\nFor commands enter 'cmds'.\n")
+    print_the_data()
+    print("\nFor commands enter 'cmds'.\n")
 
-	while True:
-		user_input = input(main_input_message)
-		if not user_input: continue
+    while True:
+        user_input = input(main_input_message)
+        if not user_input:
+            continue
 
-		if user_input.lower() == "cmds":
-			print_the_data()
-			print()
-			continue
+        if user_input.lower() == "cmds":
+            print_the_data()
+            print()
+            continue
 
-		if user_input.lower() == "imanage":
-			run(using_index=True, data=data)
-			continue
+        if user_input.lower() == "imanage":
+            run(using_index=True, data=data)
+            continue
 
-		if user_input.lower() == "manage":
-			run() # look into manager.py
-			continue
+        if user_input.lower() == "manage":
+            run()  # look into manager.py
+            continue
 
-		if user_input in 'exit -e e quit -q q ty -ty thankyou'.split() + ['thank you']:
-			raise KeyboardInterrupt # Smart move, Amr.
+        if user_input in 'exit -e e quit -q q ty -ty thankyou'.split() + ['thank you']:
+            raise KeyboardInterrupt  # Smart move, Amr.
 
-		executer(user_input)
+        executer(user_input)
+
 
 try:
-	main()
+    main()
 except KeyboardInterrupt:
-	print(wrap_string("\nGoodbye, Amr.", front=Fore.YELLOW + Style.BRIGHT))
-	exit(0)
+    print(wrap_string("\nGoodbye, Amr.", front=Fore.YELLOW + Style.BRIGHT))
+    exit(0)
